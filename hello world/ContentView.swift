952e96c3b507c5b328a98ee41b7594226e554365
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-12-12 18:37:15 -0500

    first xcode project, DONE!

--- hello world/ContentView.swift
+++ hello world/ContentView.swift
@@ -8,9 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            
+            Image("background").ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card"+String(cpuRand)
+                    
+                    if(playerRand>cpuRand){
+                        playerScore+=1
+                    }
+                    else{
+                        cpuScore+=1
+                    }
+                    
+                }) {
+                    Text("Deal")
+                }
+                .padding(.all)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player1")
+                        Text(String(playerScore))
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                        Text(String(cpuScore))
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            
+            
+        }
+        
+        
     }
 }
 
