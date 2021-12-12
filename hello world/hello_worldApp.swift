88afdaf179e2c7018a0377ade985b3972d89da35
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-12-11 20:34:56 -0500

    Initial Commit

---
+++ hello world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello world
+//
+//  Created by Nima Pourjafar on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
